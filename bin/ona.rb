0ef7965ae4c2c36dd32361344a6afa14fbad5179
Christopher Huhn <C.Huhn@gsi.de>
2017-06-13 13:27:21 +0200

    Rubocop was here

--- bin/ona.rb
+++ bin/ona.rb
@@ -5,52 +5,49 @@
 # TODO: Read config items from a config file
 #
 
-
 require 'io/console'
 require 'ona'
 require 'optparse'
 
-
 #### Commandline processing
 
-options = { :debug => 0, :params => { }, :url => 'http://localhost/ona/dcm.php'}
+options = { debug: 0, params: {}, url: 'http://localhost/ona/dcm.php' }
 
 OptionParser.new do |opts|
-  opts.banner = "Usage: ona.rb <action> <options>"
+  opts.banner = 'Usage: ona.rb <action> <options>'
 
-  opts.on("-l", "--login USER", "Username for ONA connection") do |v|
+  opts.on('-l', '--login USER', 'Username for ONA connection') do |v|
     options[:username] = v
   end
 
-  opts.on("-p", "--password PASS", "Password for ONA connection") do |v|
+  opts.on('-p', '--password PASS', 'Password for ONA connection') do |v|
     options[:password] = v
   end
 
-  opts.on("-u", "--url URL", "URL to dcm.php") do |v|
+  opts.on('-u', '--url URL', 'URL to dcm.php') do |v|
     options[:url] = v
   end
 
   opts.on('-L', '--list', 'list available modules') do
     options[:module] = 'get_module_list'
-    options[:params] = { :type => 'string' }
+    options[:params] = { type: 'string' }
   end
 
   opts.on('-r', '--run MODULE', 'run specified module') do |v|
     options[:module] = v
   end
 
-  opts.on("-t", "--test", "Debug dry-run mode") do
+  opts.on('-t', '--test', 'Debug dry-run mode') do
     options[:debug] += 1
   end
-
 end.parse!
-
 
 # turn key1=value1 key2=value2 ... cmdline args into a Ruby hash:
 unless ARGV.empty?
-  options[:params] = ARGV.inject({}) do |h,e|
+  options[:params] = ARGV.each_with_object({}) do |h, e|
     # there must be an easier way ...
-    a = e.split('='); h[a[0]] = a[1..-1].join('=');
+    a = e.split('=')
+    h[a[0]] = a[1..-1].join('=')
     h
   end
 end
@@ -58,7 +55,7 @@ end
 puts options.inspect if options[:debug] > 0
 
 # Time to ask for a password unless given on the cmdline:
-if options[:username] and not options[:password]
+if options[:username] && !(options[:password])
   STDERR.print "Password for #{options[:username]}: "
   options[:password] = STDIN.noecho(&:gets).chomp
   STDERR.puts
@@ -66,8 +63,8 @@ end
 
 # fallback to --list of no module was given
 unless options[:module]
-    options[:module] = 'get_module_list'
-    options[:params][:type] = 'string'
+  options[:module] = 'get_module_list'
+  options[:params][:type] = 'string'
 end
 
 ona = ONA.new(options[:url], options[:username], options[:password])
--- ona.gemspec
+++ ona.gemspec
@@ -2,11 +2,11 @@ Gem::Specification.new do |s|
   s.name        = 'opennetadmin'
   s.version     = '0.0.4'
   s.date        = '2015-02-06'
-  s.summary     = "Client interface to OpenNetAdmin as a replacement for dcm.pl"
-  s.author      = "Christopher Huhn"
+  s.summary     = 'Client interface to OpenNetAdmin as a replacement for dcm.pl'
+  s.author      = 'Christopher Huhn'
   s.email       = 'c.huhn@gsi.de'
-  s.files       = ["lib/ona.rb"]
-  s.executables = ["ona.rb"]
+  s.files       = ['lib/ona.rb']
+  s.executables = ['ona.rb']
   s.homepage    = 'http://gitorious.gsi.de/ona/ona_gem'
   s.license     = 'LGPL-3.0'
 end
